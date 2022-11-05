6a64d0bb36131aea7d405c4d47b90aa496b10ba5
Gegardus <stgegard@gmail.com>
2022-11-05 21:13:25 +0400

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,29 @@
+class StaticPagesController < ApplicationController
+  require 'flickr'
+
+  def index      
+      flickr = Flickr.new ENV['API_KEY'], ENV['SECRET_KEY']
+
+      unless params[:flickr_id].blank?
+          begin 
+              user = flickr.people.findByUsername :username => params[:flickr_id]
+              flickr_id = user.id
+          rescue
+              flickr_id = params[:flickr_id]
+          ensure
+              begin
+                  @pictures_list = flickr.people.getPublicPhotos :user_id => flickr_id
+                  @user = flickr.people.getInfo :user_id => flickr_id
+              rescue => e
+                  message = e.message.split("-")
+                  flash[:alert] = message[1] + " - Please Try Again"
+              end
+          end
+      else
+          if params.has_key?(:flickr_id)
+              flash[:alert] = "Please Enter a Valid User ID" 
+          end
+      end
+  end
+
+end
